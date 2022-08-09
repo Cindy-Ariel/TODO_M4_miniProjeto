59515e634b21a806303b73079c88e41a3a8b6faa
Cindy <94567136+Cindy-Ariel@users.noreply.github.com>
2022-08-09 17:48:12 -0300

    mini projeto M4

---
+++ src/controllers/dica.js
@@ -0,0 +1,10 @@
+import express from "express";
+var app = express();
+
+app.post('/create', function(req, res) {
+    res.send('Olá Mundo!');
+  })
+
+app.get'/tips', function(req, res) {
+    res.send('Olá Mundo!');
+  })
